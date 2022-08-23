92364466a36d7ebd55b7e6fa7e613fbbf700abc5
Casey McGrath <mcgrathc@hawaii.edu>
2022-08-22 21:04:14 -0400

    add nldas function

--- WRFstats_script.py
+++ WRFstats_script.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 import xarray as xr
 import timeit
@@ -9,247 +8,241 @@ import salem as sl
 # start timer
 start_time = timeit.default_timer()
 
-# set pandas display options
-pd.set_option("display.expand_frame_repr", False)
+
+# comment
 
 
-#%% function for opening one month of data
+# %% function for opening one month of data
 
 def one_month_data(path, year, month):
-    
+    """
+    Function for opening one month of data including overlapping files.
+
+    Input
+    ----------
+    path : Str Path to netCDF files for analysis.
+    year : Str Year of data for files to open.
+    month : Str Month of data for files to open.
+
+    Returns
+    -------
+    onemonthdata : List of netCDF files in specified month and year.
+
+    """
+
     # collect all files of a given month and year
     monthdata = sorted(glob(os.path.join(path, f"wrfout_*{year}-{month}*.nc")))
-    
+
     # find the following and preceeding months and year
     month_minus, year_minus = previous_file(year, month)
-    
+
     # take the file that preceeds the specified month and add to collected files, return sorted list
     last_month = sorted(glob(os.path.join(path, f"wrfout_*{year_minus}-{month_minus}*.nc")))[-1]
     monthdata.append(last_month)
     onemonthdata = sorted(monthdata)
-    
+
     return onemonthdata
 
 
 # function to find the previous file containing parts of the given month
 def previous_file(year, month):
-    
     months_list = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
     idx = months_list.index(month)
 
-    month_minus = months_list[idx - 1]
-    
+    month_minus = months_list[idx - 1]  # get previous mnonth
+
     # if month is January, return previous year, else current year
     if month == "01":
         year_minus = str(int(year) - 1)
     else:
         year_minus = year
-    
+
     return month_minus, year_minus
 
 
-#%% function for calculating rolling max and min
+# %% function for calculating rolling max and min
 
-def WRFminmax(max_df, min_df, max_roll, min_roll, n):
-    
+def WRFminmax(max, min, max_roll, min_roll, n):
     """
     Function for calculating rolling maximum and minimum values for each variable.
-    
-    Parameters
+
+    Input
     ----------
-    max_df : DataFrame
-        Pandas DataFrame with maximum values for current file.
-    min_df : DataFrame
-        Pandas DataFrame with minimum values for current file.
-    max_roll : DataFrame
-        Pandas DataFrame with rolling maximum values.
-    min_roll : DataFrame
-        Pandas DataFrame with rolling minimum values.
-    n : Int
-        Current value of iteration counter.
+    max : DataSet Maximum values for current file.
+    min : DataSet Minimum values for current file.
+    max_roll : DataSet Rolling maximum values.
+    min_roll : DataSet Rolling minimum values.
+    n : Int Current value of iteration counter.
+
     Returns
     -------
-    max_roll, min_roll : Series
-        Pandas DataFrame for storage of maximum and minimum values.
-    
+    max_roll, min_roll : DataSets for storage of maximum and minimum values.
+
     """
+
     ## check if this is the first file in the run, if so then assign values from current dataset
     if n == 0:
-        max_roll = max_df.reset_index(drop = True)
-        min_roll = min_df.reset_index(drop = True)
-        
+        max_roll = max
+        min_roll = min
+
     else:
         # check if new max is greater than old max, if so then replace it, if none are greater then skip loop
-        if (max_df.reset_index(drop = True) > max_roll).any():
-            for i in range(len(max_df)):
-                if max_df.reset_index(drop = True)[i] > max_roll[i]:
-                    max_roll[i] = max_df.reset_index(drop = True)[i]
-        
+        if (max > max_roll).any():
+            max_roll = xr.where(max > max_roll, max, max_roll)
+
         # check if new min is less than old min, if so then replace it, if none are less then skip loop
-        if (min_df.reset_index(drop = True) < min_roll).any():
-            for i in range(len(min_df)):
-                if min_df.reset_index(drop = True)[i] < min_roll[i]:
-                    min_roll[i] = min_df.reset_index(drop = True)[i]
-    
+        if (min < max_roll).any():
+            min_roll = xr.where(min < min_roll, min, min_roll)
+
     return max_roll, min_roll
 
 
-#%% function for calculating cumulative standard deviation and sample size
+# %% function for calculating cumulative standard deviation and sample size
 
-def WRFstddev(stddev_df, stddev_roll, sample_size_series, sample_size_roll, n):
-    
+def WRFstddev(stddev, sample_size, stddev_roll, sample_size_roll, n):
     """
     Function for calculating pooled standard deviation and rolling sample size values for each variable.
-    
-    Parameters
+
+    Input
     ----------
-    stddev_df : DataFrame
-        Pandas DataFrame with standard deviation values for current file.
-    stddev_roll : DataFrame
-        Pandas DataFrame with rolling standard deviation values.
-    sample_size_series : Series
-        Pandas series with sample sizes of each variable for current file.
-    sample_size_roll : Series
-        Pandas series with rolling sample sizes of each variable.
-    n : Int
-        Current value of iteration counter.
+    stddev : DataSet Standard deviation values for current file.
+    stddev_roll : DataSet Rolling standard deviation values.
+    sample_size : DataSet Sample sizes of each variable for current file.
+    sample_size_roll : DataSet Rolling sample sizes of each variable.
+    n : Int Current value of iteration counter.
+
     Returns
     -------
-    stddev_roll, sample_size_roll : Series
-        Pandas series for storage of standard deviation and sample size values.
-    
+    stddev_roll, sample_size_roll : DataSets for storage of rolling standard deviation and sample size values.
+
     """
-    
+
     # check if this is the first file in the run, if so then assign values from current dataset
     if n == 0:
-        sample_size_roll = sample_size_series
-        stddev_roll = stddev_df
+        sample_size_roll = sample_size
+        stddev_roll = stddev
     else:
-        sample_size_roll = sample_size_roll + sample_size_series
+        sample_size_roll = sample_size_roll + sample_size
 
         # Cohen pooled method of combining weighted std devs, assuming similar variances in samples
-        sd1 = stddev_roll.reset_index(drop = True)
-        sd2 = stddev_df.reset_index(drop = True)
-        n1 = sample_size_roll.reset_index(drop = True)
-        n2 = sample_size_series.reset_index(drop = True)
-        stddev_roll = np.sqrt( ( ((n1 - 1) * sd1**2) + ((n2 - 1) * sd2**2 ) ) / (n1 + n2 - 2) )
+        sd1 = stddev_roll
+        sd2 = stddev
+        n1 = sample_size_roll
+        n2 = sample_size
+        stddev_roll = np.sqrt((((n1 - 1) * sd1 ** 2) + ((n2 - 1) * sd2 ** 2)) / (n1 + n2 - 2))
 
     return stddev_roll, sample_size_roll
 
 
-#%% function to combine and deaccumulate precipitation variables into new variable
+# %% function to combine and deaccumulate precipitation variables into new variable
 
 def deacc_precip(ds, ds_variables):
-    
+    # check if the variable PRECIP was included in the variables list, if so then create variable from rain variables and deaccumulate
     if "PRECIP" in ds_variables:
         ds["PRECIP"] = ds["RAINC"] + ds["RAINSH"] + ds["RAINNC"]
-        ds["PRECIP"].values = np.diff(ds["PRECIP"].values, axis = 0, prepend = np.array([ds["PRECIP"][0].values]))
-        
-    ds["RAINC"].values = np.diff(ds["RAINC"].values, axis = 0, prepend = np.array([ds["RAINC"][0].values]))
-    ds["RAINSH"].values = np.diff(ds["RAINSH"].values, axis = 0, prepend = np.array([ds["RAINSH"][0].values]))
-    ds["RAINNC"].values = np.diff(ds["RAINNC"].values, axis = 0, prepend = np.array([ds["RAINNC"][0].values]))    
+        ds["PRECIP"].values = np.diff(ds["PRECIP"].values, axis=0, prepend=np.array([ds["PRECIP"][0].values]))
+
+    # deaccumulate rain variables
+    ds["RAINC"].values = np.diff(ds["RAINC"].values, axis=0, prepend=np.array([ds["RAINC"][0].values]))
+    ds["RAINSH"].values = np.diff(ds["RAINSH"].values, axis=0, prepend=np.array([ds["RAINSH"][0].values]))
+    ds["RAINNC"].values = np.diff(ds["RAINNC"].values, axis=0, prepend=np.array([ds["RAINNC"][0].values]))
 
 
-#%% function for aggregating rolling stats on netCDF data
+# %% function for calculating magnitude of velocity vectors
 
-def WRFstats(path, year, month, 
-             ds_variables=["LU_INDEX","Q2","T2","PSFC","U10","V10","SFROFF","UDROFF","ACSNOM","SNOW","SNOWH","WSPD","BR",
-                           "ZOL","RAINC","RAINSH","RAINNC","PRECIP","SNOWNC","GRAUPELNC","HAILNC","SWDOWN","GLW","UST","SNOWC","SR"]
+def magnitude(ds):
+    U = ds["U10"]
+    V = ds["V10"]
+    ds["WINDSPEED"] = np.sqrt(U ** 2 + V ** 2)
+
+
+# %% function for aggregating rolling stats on netCDF data
+
+def WRFstats(path, year, month,
+             ds_variables=["LU_INDEX", "Q2", "T2", "PSFC", "U10", "V10", "WINDSPEED", "SFROFF", "UDROFF", "ACSNOM",
+                           "SNOW", "SNOWH", "WSPD", "BR",
+                           "ZOL", "RAINC", "RAINSH", "RAINNC", "PRECIP", "SNOWNC", "GRAUPELNC", "HAILNC", "SWDOWN",
+                           "GLW", "UST", "SNOWC", "SR"]
              ):
-    
     """
     Function for running moving (rolling) descriptive statistics on all netCDF files at a given location.
-    
-    Parameters
+
+    Input
     ----------
-    path : Str
-        Path to netCDF files for analysis.
+    path: Str Path to netCDF files for analysis.
+    year: Str Year of data for files to open.
+    month: Str Month of data for files to open.
+    ds_variables: List Variables to run stats on.
+
     Returns
     -------
-    stats_df : DataFrame
-        Pandas DataFrame for storage of stats.
-    
+    mean_roll: DataSet netCDF file for storage of rolling mean.
+    stddev_roll: DataSet netCDF file for storage of rolling standard deviation.
+    max_roll: DataSet netCDF file for storage of rolling maximums.
+    min_roll: DataSet netCDF file for storage of rolling minimums.
+
     """
-    
+
     # create list of netCDF files at path
-    #nc_files = [file for file in os.listdir(path) if file.endswith(".nc")]
+    # nc_files = [file for file in os.listdir(path) if file.endswith(".nc")]
     nc_files = one_month_data(path, year, month)
-    
+
     # create rolling stats variables, set intial values
-    n = 0 # counter
+    n = 0  # counter
     sample_size_roll = None
     mean_roll = 0
     stddev_roll = None
     median_roll = 0
     max_roll = None
     min_roll = None
-    
+
     # iterate through each nc file and create dataset
     for file in nc_files:
-        
-        ds_sl = sl.open_wrf_dataset(file) # open netCDF data path and create xarray dataset using salem
-        ds = ds_sl.sel(time = f"{year}-{month}") # slice data by year and month
-        
+        ds_sl = sl.open_wrf_dataset(file)  # open netCDF data path and create xarray dataset using salem
+        ds = ds_sl.sel(time=f"{year}-{month}")  # slice data by year and month
+
         # combine and deaccumulate precipitation variables into new variable
         deacc_precip(ds, ds_variables)
-        
+
+        # create new variable for wind speed from magnitudes of velocity vectors
+        magnitude(ds)
+
         # calculate descriptive stats on file using xarray
-        mean = ds[ds_variables].mean()
-        stddev = ds[ds_variables].std()
-        median = ds[ds_variables].median()
-        max = ds[ds_variables].max()
-        min = ds[ds_variables].min()
-        
-        # convert stats to pandas for storage
-        mean_df = mean.to_pandas().reset_index(drop = True)
-        stddev_df = stddev.to_pandas()
-        median_df = median.to_pandas().reset_index(drop = True)
-        max_df = max.to_pandas()
-        min_df = min.to_pandas()
-        
-        #var_names = [key for key in ds.variables.keys()][3:] # extract variable names for index matching
-        sample_size = [eval(f"ds.{var}.size", {"ds": ds}) for var in ds_variables] # find size of each variable
-        sample_size_series = pd.Series(data = sample_size, dtype = float) # convert to pandas series for calculation in formula
-        
+        mean = ds[ds_variables].mean(dim="time", skipna=True)
+        stddev = ds[ds_variables].std(dim="time", skipna=True)
+        median = ds[ds_variables].median(dim="time", skipna=True)
+        max = ds[ds_variables].max(dim="time", skipna=True)
+        min = ds[ds_variables].min(dim="time", skipna=True)
+        sample_size = ds[ds_variables].count(dim="time")
+
         # aggregate means using cumulative moving average method
-        mean_roll = (mean_df + (n * mean_roll)) / (n + 1)
-        
+        mean_roll = (mean + (n * mean_roll)) / (n + 1)
+
         # function for calculating rolling std dev and cumulative sample size
-        stddev_roll, sample_size_roll = WRFstddev(stddev_df, stddev_roll, sample_size_series, sample_size_roll, n)
-        
+        stddev_roll, sample_size_roll = WRFstddev(stddev, sample_size, stddev_roll, sample_size_roll, n)
+
         # function for calculating rolling max and min
-        max_roll, min_roll = WRFminmax(max_df, min_df, max_roll, min_roll, n)
-        
-        #TODO cumulative methods for median
-        median_roll = (median_df + (n * median_roll)) / (n + 1)
-        
-        n += 1 # iterate counter
-        
-        print("Run: ", n) # just to make sure it's still working...
+        max_roll, min_roll = WRFminmax(max, min, max_roll, min_roll, n)
+
+        # TODO cumulative methods for median
+        median_roll = (median + (n * median_roll)) / (n + 1)
+
+        n += 1  # iterate counter
+
+        print("Run: ", n)  # just to make sure it's still working...
         print("Current Runtime: ", timeit.default_timer() - start_time, "\n")
-        
-    # create dictionary of stats and convert to DataFrame
-    stats = {
-            "Variable": ds_variables,
-            "Mean": mean_roll,
-            "Standard Dev.": stddev_roll,
-            "Median": median_roll,
-            "Maximum": max_roll,
-            "Minimum": min_roll
-            }
-    
-    stats_df = pd.DataFrame(data = stats, index = None)
-    
-    return stats_df
+
+    return mean_roll, stddev_roll, max_roll, min_roll
 
 
-#%% run code
+# %% run code
 
 path = r"C:\Users\mart229\OneDrive - PNNL\Desktop\Stuff\IM3\WRF\Month_Data\\"
 year = "2007"
 month = "01"
 
-WRFstats = WRFstats(path, year, month)
+# WRFstats = WRFstats(path, year, month)
+mean_ds, stddev_ds, max_ds, min_ds = WRFstats(path, year, month)
 
-print(WRFstats)
-print("\n", "Total Runtime: ", timeit.default_timer() - start_time) # end timer and print
+# print(WRFstats)
+print("\n", "Total Runtime: ", timeit.default_timer() - start_time)  # end timer and print
---
+++ nldas_stats.py
@@ -0,0 +1,237 @@
+import numpy as np
+import xarray as xr
+import os
+from glob import glob
+from datetime import datetime, timedelta
+
+
+# %% function for opening a variable range of data
+
+def variable_range_data(input_path, start, stop):
+    """
+    Function for opening a variable range of files between speicifed dates (inclusive).
+
+    Input
+    ----------
+    input_path : Str Path to netCDF files for analysis.
+    start : Str "YYYY-MM-DD" Start date of files to open.
+    stop : Str "YYYY-MM-DD" End date of files to open.
+
+    Returns
+    -------
+    rangefiles : List of netCDF files in specified date range.
+
+    """
+
+    # find the starting year, month, and day
+    startyear = start[0: start.find("-")]
+    startmonth = start[start.find("-") + 1: start.rfind("-")]
+    startday = start[start.rfind("-") + 1:]
+
+    # find the ending year, month, and day
+    stopyear = stop[0: stop.find("-")]
+    stopmonth = stop[stop.find("-") + 1: stop.rfind("-")]
+    stopday = stop[stop.rfind("-") + 1:]
+
+    # collect all files for the years in the given range
+    years = [str(int(startyear) + i) for i in range(int(stopyear) - int(startyear) + 1)]
+    yearfiles_list = [sorted(glob(os.path.join(input_path, f"NLDAS_FORA0125_H.A*{year}*.002.grb.SUB.nc4"))) for year in years]
+    yearfiles = [file for list in yearfiles_list for file in list]
+
+    # calculate the time difference in the given date range
+    startdate = datetime.strptime(start, "%Y-%m-%d")
+    stopdate = datetime.strptime(stop, "%Y-%m-%d")
+    delta = stopdate - startdate
+
+    # create list of all dates between the given range of dates
+    filedates = [str((startdate + timedelta(days=d)).strftime("%Y%m%d")) for d in range(delta.days + 1)]
+
+    # collect just the files between the date range that are present in the folder
+    rangefiles = [glob(os.path.join(input_path, f"NLDAS_FORA0125_H.A*{date}*.002.grb.SUB.nc4"))[0] for date in filedates if
+                  date in str(yearfiles)]
+
+    return rangefiles
+
+
+# %% function for calculating rolling max and min
+
+def NLDASminmax(max, min, max_roll, min_roll, n):
+    """
+    Function for calculating rolling maximum and minimum values for each variable.
+
+    Input
+    ----------
+    max : DataSet Maximum values for current file.
+    min : DataSet Minimum values for current file.
+    max_roll : DataSet Rolling maximum values.
+    min_roll : DataSet Rolling minimum values.
+    n : Int Current value of iteration counter.
+
+    Returns
+    -------
+    max_roll, min_roll : DataSets for storage of maximum and minimum values.
+
+    """
+
+    ## check if this is the first file in the run, if so then assign values from current dataset
+    if n == 0:
+        max_roll = max
+        min_roll = min
+
+    else:
+        # check if new max is greater than old max, if so then replace it, if none are greater then skip loop
+        if (max > max_roll).any():
+            max_roll = xr.where(max > max_roll, max, max_roll)
+
+        # check if new min is less than old min, if so then replace it, if none are less then skip loop
+        if (min < max_roll).any():
+            min_roll = xr.where(min < min_roll, min, min_roll)
+
+    return max_roll, min_roll
+
+
+# %% function for calculating cumulative standard deviation and sample size
+
+def NLDASstddev(stddev, sample_size, stddev_roll, sample_size_roll, n):
+    """
+    Function for calculating pooled standard deviation and rolling sample size values for each variable.
+
+    Input
+    ----------
+    stddev : DataSet Standard deviation values for current file.
+    stddev_roll : DataSet Rolling standard deviation values.
+    sample_size : DataSet Sample sizes of each variable for current file.
+    sample_size_roll : DataSet Rolling sample sizes of each variable.
+    n : Int Current value of iteration counter.
+
+    Returns
+    -------
+    stddev_roll, sample_size_roll : DataSets for storage of rolling standard deviation and sample size values.
+
+    """
+
+    # check if this is the first file in the run, if so then assign values from current dataset
+    if n == 0:
+        sample_size_roll = sample_size
+        stddev_roll = stddev
+    else:
+        sample_size_roll = sample_size_roll + sample_size
+
+        # Cohen pooled method of combining weighted std devs, assuming similar variances in samples
+        sd1 = stddev_roll
+        sd2 = stddev
+        n1 = sample_size_roll
+        n2 = sample_size
+        stddev_roll = np.sqrt((((n1 - 1) * sd1 ** 2) + ((n2 - 1) * sd2 ** 2)) / (n1 + n2 - 2))
+
+    return stddev_roll, sample_size_roll
+
+
+
+# %% function for calculating magnitude of velocity vectors
+
+def magnitude(ds):
+    U = ds["UGRD"]
+    V = ds["VGRD"]
+    ds["WINDSPEED"] = np.sqrt(U ** 2 + V ** 2)
+
+
+# %% function for aggregating rolling stats on netCDF data
+
+def NLDASstats(input_path, output_path, start, stop,
+             ds_variables=["TMP", "SPFH", "PRES", "UGRD", "VGRD", "DLWRF",
+                             "PEVAP", "APCP", "DSWRF"]
+             ):
+    """
+    Function for running moving (rolling) descriptive statistics on all netCDF files between a given range of dates.
+
+    Input
+    ----------
+    input_path : Str Path to netCDF files for analysis.
+    output_path : Str Path for the output netCDF files to be stored.
+    year : Str Year of data for files to open.
+    month : Str Month of data for files to open.
+    ds_variables : List Variables to run stats on.
+
+    Returns
+    -------
+    mean_roll : DataSet netCDF file for storage of rolling mean.
+    avg_median_roll : DataSet netCDF file for storage of rolling average median.
+    stddev_roll : DataSet netCDF file for storage of rolling standard deviation.
+    max_roll : DataSet netCDF file for storage of rolling maximums.
+    min_roll : DataSet netCDF file for storage of rolling minimums.
+
+    """
+
+    # create list of netCDF files at path
+    nc_files = variable_range_data(input_path, start, stop)
+
+    # create rolling stats variables, set intial values
+    n = 0  # counter
+    mean_roll = 0
+    avg_median_roll = 0
+    stddev_roll = None
+    sample_size_roll = None
+    max_roll = None
+    min_roll = None
+
+    # iterate through each nc file and create dataset
+    for file in nc_files:
+
+        ds = xr.open_dataset(file)  # open netCDF data path and create xarray dataset using salem
+
+        # check if last file in run, if so then slice data by stop date
+        if file == nc_files[-1]:
+            ds = ds.sel(time=slice(f"{stop}"))
+
+
+        # create new variable for wind speed from magnitudes of velocity vectors
+        magnitude(ds)
+
+        # calculate descriptive stats on file using xarray
+        mean = ds[ds_variables].mean(dim="time", skipna=True)
+        stddev = ds[ds_variables].std(dim="time", skipna=True)
+        avg_median = ds[ds_variables].median(dim="time", skipna=True)
+        max = ds[ds_variables].max(dim="time", skipna=True)
+        min = ds[ds_variables].min(dim="time", skipna=True)
+        sample_size = ds[ds_variables].count(dim="time")
+
+        # aggregate means using cumulative moving average method
+        mean_roll = (mean + (n * mean_roll)) / (n + 1)
+
+        # aggregate average medians using cumulative moving average method
+        avg_median_roll = (avg_median + (n * avg_median_roll)) / (n + 1)
+
+        # function for calculating rolling std dev and cumulative sample size
+        stddev_roll, sample_size_roll = NLDASstddev(stddev, sample_size, stddev_roll, sample_size_roll, n)
+
+        # function for calculating rolling max and min
+        max_roll, min_roll = NLDASminmax(max, min, max_roll, min_roll, n)
+
+
+        n += 1  # iterate counter
+
+    # specify the location for the output of the program
+    output_filename = os.path.join(output_path + f"{start}_{stop}_")
+
+    # save each output stat as a netCDF file
+    mean_roll.to_netcdf(path=output_filename + "Mean_DS.nc")
+    avg_median_roll.to_netcdf(path=output_filename + "Avg_Median_DS.nc")
+    stddev_roll.to_netcdf(path=output_filename + "StdDev_DS.nc")
+    max_roll.to_netcdf(path=output_filename + "Max_DS.nc")
+    min_roll.to_netcdf(path=output_filename + "Min_DS.nc")
+
+    return mean_roll, avg_median_roll, stddev_roll, max_roll, min_roll
+
+
+# %% run code
+
+# specify the path to the location of the files to be analyzed,
+# the path for the output to be stored, and the start and stop dates
+input_path = "/global/cscratch1/sd/mcgrathc/"
+output_path = "/global/cscratch1/sd/mcgrathc/nldas_output"
+start = "1980-01-01"
+stop = "1989-12-31"
+
+# run the WRFstats program
+mean_ds, avg_median_ds, stddev_ds, max_ds, min_ds = NLDASstats(input_path, output_path, start, stop)
